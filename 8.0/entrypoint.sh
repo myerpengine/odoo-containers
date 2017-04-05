f4351211046b4fe1b0733b5782b61c5f54557aec
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2017-04-05 19:10:57 +0200

    Modified entry points

--- 8.0/entrypoint.sh
+++ 8.0/entrypoint.sh
@@ -2,13 +2,17 @@
 
 if [ ! -f /inited ]; then
 
-    echo "Setting user's uid to $USER_UID"
-    usermod -u $USER_UID user
-    chown -R user:user /home/user
+    if [ "$USER_UID" != "" ]; then
+        echo "Setting user's uid to $USER_UID"
+        usermod -u $USER_UID user
+        chown -R user:user /home/user
+    fi
     
-    echo "Adding public key to authorized keys"
-    mkdir -p /root/.ssh
-    echo $PUBLIC_AUTH_KEY >> /root/.ssh/authorized_keys2
+    if [ "$PUBLIC_AUTH_KEY" != "" ]; then
+        echo "Adding public key to authorized keys"
+        mkdir -p /root/.ssh
+        echo $PUBLIC_AUTH_KEY >> /root/.ssh/authorized_keys2
+    fi
     
     echo "Resetting password of root"
     echo root:`pwgen -cnsN 1 12` | chpasswd
@@ -17,7 +21,6 @@ if [ ! -f /inited ]; then
     echo user:`pwgen -cnsN 1 12` | chpasswd
     
     touch /inited
-
 fi
 
 # for legacy purpose
